b31231a5b3a17a4881972523298ea77d165dd6a0
IsmaelJDz <Ismael_br7@hotmail.com>
2017-03-31 14:51:08 -0600

    maquetado 100% y consumiendo la API de tvMaze

--- js/tvify.js
+++ js/tvify.js
@@ -1,7 +1,46 @@
 $(function() {
-    var header = $('#app-header h1')
-    var header2 = document.getElementById('app-header')
-    var seleccion = $([ document, header ]);
 
-    $('header[data-title='TVify']')
+    /**
+    * Submit search form
+    */
+
+    function onsubmit( evt ) {
+        /* Act on the event */
+        evt.preventDefault();
+        var busqueda = $(this)
+            .find('input[type="text"]')
+            .val();
+            alert('se ha buscado ' + busqueda );
+    }
+
+    var template = '<article class="tv-show">' +
+        '<div class="left img-container">'+
+            '<img src=":img:" alt=":img alt:">'+
+        '</div>'+
+        '<div class="right info">'+
+            '<h1>:name:</h1>'+
+            '<p>:summary:</p>'+
+        '</div>'+
+    '</article>';
+
+    $('#app-body')
+        .find('form')
+        .submit(onsubmit);
+
+    $.ajax({
+        url: 'http://api.tvmaze.com/shows',
+        success: function(shows, textStatus, xhr){
+            var $tvShowsContainer = $('#app-body').find('.tv-shows');
+            shows.forEach(function (show) {
+                var article = template
+                    .replace(':name:', show.name)
+                    .replace(':img:', show.image.medium)
+                    .replace(':summary:', show.summary)
+                    .replace(':img alt:', show.name +  " Logo")
+
+                    $tvShowsContainer.append($(article))
+            });
+        }
+    });
+
 });
